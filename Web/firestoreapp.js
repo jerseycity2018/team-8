d3b0336dbf69ba3e62af035f55fc6862b5368b7c
Admin <admin@Admin.local>
2018-10-27 03:36:13 -0400

    fixed firebase forreal

--- Web/firestoreapp.js
+++ Web/firestoreapp.js
@@ -11,17 +11,40 @@ firebase.initializeApp(config);
 
 
 var firestore = firebase.firestore();
+const settings = ({timestampsInSnapshots: true });
+  firestore.settings(settings);
+
 
 const docRef = firestore.collection("users").doc("VTpqXygWydQfF3lswb98");
 
 // Event listener to get DB data
-Retrieve.addEventListener("click", function() {
-  docRef.get().then(function (doc) {
-    if (doc && doc.exists) {
-      const myData = doc.data();
-         }
-      }).catch(function (error) {
-     console.log("Got an error: ", error);
-    // })
-  })
-})
+
+const getUserData = function(data) {
+  const collection = firebase.firestore().collection('users').get();
+  return collection;
+};
+
+// Retrieve.addEventListener("click", function() {
+//   docRef.get().then(function (doc) {
+//     if (doc && doc.exists) {
+//       const myData = doc.data();
+//          }
+//       }).catch(function (error) {
+//      console.log("Got an error: ", error);
+//     // })
+//   })
+// })
+
+
+const getUserFireStoreData = document.querySelector("#Retrieve");
+firestore.collection("users").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+
+        //add this data to your div's textView
+        console.log(doc.data().name)
+        //console.log(`${doc.id} => ${doc.data()}`);
+    });
+});
+
+
+//const errorOverride = firebase.firestore()
