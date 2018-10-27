ba7c969f9d68042d7b2538f6824210e1f0809529
stanmill <stanley.pena29@gmail.com>
2018-10-27 01:32:55 -0400

    intents

--- CPCApp/app/src/main/java/com/example/stanleypena/cpcapp/MainActivity.java
+++ CPCApp/app/src/main/java/com/example/stanleypena/cpcapp/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.stanleypena.cpcapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button camera;
+    Button urcode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        camera = (Button) findViewById(R.id.camerab);
+        urcode = (Button) findViewById(R.id.urb);
+
+
+
+    }
+
+    public void actionButton(View view) {
+        Intent urintent = new Intent(this, QRScanner.class);
+        startActivity(urintent);
+
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivity(intent);
+
     }
 }
