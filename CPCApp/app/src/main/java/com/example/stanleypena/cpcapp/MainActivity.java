9e3e9eae6cbc00cc26f92dd5cb45a595726b001b
stanmill <stanley.pena29@gmail.com>
2018-10-27 04:44:05 -0400

    camera info

--- CPCApp/app/src/main/java/com/example/stanleypena/cpcapp/MainActivity.java
+++ CPCApp/app/src/main/java/com/example/stanleypena/cpcapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.stanleypena.cpcapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +11,8 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     Button camera;
     Button urcode;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +22,27 @@ public class MainActivity extends AppCompatActivity {
         urcode = (Button) findViewById(R.id.urb);
 
 
+    }
+
+    public void actionButton(View view){
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            //bellow the value will be stored in a image view....but u gotta decide wat to do with that data
+            //mImageView.setImageBitmap(imageBitmap);
+        }
+    }
 
     }
 
-    public void actionButton(View view) {
-        Intent urintent = new Intent(this, QRScanner.class);
-        startActivity(urintent);
 
-        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-        startActivity(intent);
-
-    }
-}
