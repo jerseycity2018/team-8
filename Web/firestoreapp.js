ecd12ad5867d07bf9fe45f80d3af58034137464c
Admin <admin@Admin.local>
2018-10-27 00:53:38 -0400

    cleaning firebase app

--- Web/firestoreapp.js
+++ Web/firestoreapp.js
@@ -1,5 +1,5 @@
 // Initialize Firebase
-  var config = {
+var config = {
     apiKey: "",
     authDomain: "jphack-21831.firebaseapp.com",
     databaseURL: "https://jphack-21831.firebaseio.com",
@@ -7,7 +7,6 @@
     storageBucket: "jphack-21831.appspot.com",
     messagingSenderId: "893931299090"
   };
-  firebase.initializeApp(config);
 
 const db = firebase.firestore();
 // Require firestore
@@ -16,17 +15,30 @@ const firebase = require("firebase");
 require("firebase/firestore");
 
 
-// Initialize Google fireStore
+// Initialize Google firestore
 firebase.initializeApp({
   apiKey: '',
   authDomain: 'jphack-21831.firebaseapp.com',
   projectId: 'jphack-21831'
 });
 
-// Initialize Cloud Firestore through Firebase
+const docRef = db.collection("users").doc("VTpqXygWydQfF3lswb98")
+const docLocRef = db.collection("location").doc("eIJ7iOOsyBeLwvST3QP2b98")
 
+loadButton.addEventListener("click", function(){
+  docRef.get().then(function (doc) {
+    if (doc && doc.exists) {
+      const myData = doc.data();
+    }
+  }).catch(function (error) {
+    console.log("Got an error: ", error);
+  })
+});
+// Initialize Cloud Firestore through Firebase
 
 // Disable deprecated features
 db.settings({
   timestampsInSnapshots: true
 });
+
+firebase.initializeApp(config);
