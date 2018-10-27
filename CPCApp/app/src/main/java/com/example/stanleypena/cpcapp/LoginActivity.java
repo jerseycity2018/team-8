4b848b09892eb195dffaed1f5e5fb8346bf814c0
Yohji Kusakabe <Ykauji@gmail.com>
2018-10-27 10:42:51 -0400

    commit commit
    timmoc

--- CPCApp/app/src/main/java/com/example/stanleypena/cpcapp/LoginActivity.java
+++ CPCApp/app/src/main/java/com/example/stanleypena/cpcapp/LoginActivity.java
@@ -111,5 +111,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         });
 
     }
+
 }
 
--- CPCApp/app/src/main/java/com/example/stanleypena/cpcapp/QRScanner.java
+++ CPCApp/app/src/main/java/com/example/stanleypena/cpcapp/QRScanner.java
@@ -1,6 +1,7 @@
 package com.example.stanleypena.cpcapp;
 import android.content.Intent;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,30 +19,44 @@ import android.widget.Toast;
 import android.net.Uri;
 import java.io.File;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class QRScanner extends AppCompatActivity implements View.OnClickListener{
 
-    TextView textView;
-    Button scanImage;
-    BarcodeDetector barcodeDetector;
-    ImageView myImageView;
+    public TextView textView;
+    public Button scanImage;
+    public BarcodeDetector barcodeDetector;
+    public ImageView myImageView;
+    private FirebaseFirestore db;
+    private DocumentReference mDocRef;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrscanner);
+        db = FirebaseFirestore.getInstance();
         scanImage = findViewById(R.id.scanButton);
         // load image
         myImageView = (ImageView) findViewById(R.id.qrTest);
         scanImage.setOnClickListener(this);
+
     }
+
 
     @Override
     public void onClick(View v) {
@@ -60,20 +75,31 @@ public class QRScanner extends AppCompatActivity implements View.OnClickListener
             Frame frame = new Frame.Builder().setBitmap(imageBitmap).build();
             SparseArray<Barcode> barcodes = barcodeDetector.detect(frame);
 
+
             // Decode
+            for (int i = 0; i < barcodes.size(); i++) {
+
+            }
             Barcode thisCode = barcodes.valueAt(0);
             Toast.makeText(this, thisCode.rawValue, Toast.LENGTH_SHORT).show();
         }
     }
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        String potato = Integer.toString(requestCode);
-//        Log.e("test",potato);
-//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-//            Bundle extras = data.getExtras();
-//            Bitmap imageBitmap = (Bitmap) extras.get("data");
-//
-//        }
-//
-//    }
 }
+
+//    Map<String, Object> userData = new HashMap<>();
+//        userData.put("Username",userNameEditText.getText().toString().trim());
+//                userData.put("Date Created",new Date());
+//                userID = FirebaseAuth.getInstance().getCurrentUser();
+//                String userUID = userID.getUid();
+//                mDocRef = FirebaseFirestore.getInstance().document("users/" + userUID);
+//                mDocRef.set(userData).addOnSuccessListener(new OnSuccessListener<Void>() {
+//@Override
+//public void onSuccess(Void aVoid) {
+//        Log.d(TAG,"Document has been saved");
+//        }
+//        }).addOnFailureListener(new OnFailureListener() {
+//@Override
+//public void onFailure(@NonNull Exception e) {
+//        Log.e(TAG,"Document failed to save");
+//        }
+//        });
